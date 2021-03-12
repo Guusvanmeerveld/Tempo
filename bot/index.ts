00a73f6c02f9fb0e0a4d18a3fa5abf1836e33b78
Guusvanmeerveld <guusvanmeerveld@live.nl>
2021-03-12 21:04:36 +0000

    Removed node_modules check

--- bot/index.ts
+++ bot/index.ts
@@ -1,9 +1,3 @@
-import fs from 'fs';
-if (!fs.existsSync('../node_modules/')) {
-	console.log('Could not find node_modules, did you remember to run npm install?');
-	process.exit();
-}
-
 require('dotenv').config();
 
 import Manager from './src/manager';
